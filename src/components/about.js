a9cc8344cd59109e843523f345cafd025bf719b2
Corey Block <corey.block7@gmail.com>
2021-06-24 14:12:45 -0600

    components added

---
+++ src/components/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Corey Block</h2>
+            <hr></hr>
+            <div>
+                <img class="mb-5" id="avatar" src="./public/corey.jpg" alt="Corey Block" />
+
+                <p>
+                A motivated and aspiring web developer interested in continuously learning more and advancing myself in the field of programming and web development.
+                </p>
+                
+            </div>
+        </section>
+    )
+}
+
+export default About;
