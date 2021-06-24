9992d0f885b77ed5cbbd470d0fa89160028a8e8e
Corey Block <corey.block7@gmail.com>
2021-06-24 15:04:20 -0600

    fixed issue that wasn't allowing images to load

--- src/components/about.js
+++ src/components/about.js
@@ -7,7 +7,7 @@ function About() {
             <h2 class="top-title">Corey Block</h2>
             <hr></hr>
             <div>
-                <img class="mb-5" src="../../../public/corey.jpg" alt="Corey Block" />
+                <img className="mb-5" src="https://avatars.githubusercontent.com/u/80268994?v=4" alt="Corey Block" />
 
                 <p>
                 A motivated and aspiring web developer interested in continuously learning more and advancing myself in the field of programming and web development.
